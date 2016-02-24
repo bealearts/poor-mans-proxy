7180bd66e71e6b7bf04c4666b1a8a736b6b86862
David Beale <david.beale@bealearts.co.uk>
2016-02-24 13:30:04 +0000

    Add function proxy

---
+++ test/FunctionTest.js
@@ -0,0 +1,56 @@
+'use strict';
+
+require('../');
+
+/*jshint -W079 */
+var expect = require('chai').expect;
+
+
+describe('Function Proxy', function() {
+	
+
+	it('should be transparent without a handler', function() {
+
+		var target = function(arg) {
+			return arg;
+		};
+
+		var proxy = new Proxy(target);
+
+		expect(proxy(8)).to.equal(8);
+
+	});
+
+	it('should not be transparent with a handler', function() {
+
+		var target = function(arg) {
+			return arg;
+		};
+
+		var proxy = new Proxy(target, {
+			apply: function(target, thisArg, argumentsList) {
+				return 0;
+			}
+		});
+
+		expect(proxy(8)).to.equal(0);
+
+	});
+
+	it('should be transparent with a handler that delegates to the target', function() {
+
+		var target = function(arg) {
+			return arg;
+		};
+
+		var proxy = new Proxy(target, {
+			apply: function(target, thisArg, argumentsList) {
+				return target.apply(thisArg, argumentsList);
+			}
+		});
+
+		expect(proxy(8)).to.equal(8);
+
+	});
+
+});
